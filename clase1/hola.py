81c83414812211cb64aeb5cc85ab7e8ddebcceef
Luciano Martinez <lucianomartinez@gmail.com>
2020-06-02 15:07:44 -0300

    add clase1 - hola mundo

---
+++ clase1/hola.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+"""Realice un módulo (archivo hola.py) que contenga la siguiente sentencia:
+print('Hola mundo!!!'). Luego ejecute el módulo desde la consola del sistema operativo"""
+
+
+print('Hola mundo!!!')
