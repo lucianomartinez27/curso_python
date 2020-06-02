0c0058f54632845ff61bea2d1db999c48e7b396e
Luciano Martinez <lucianomartinez@gmail.com>
2020-06-02 11:04:50 -0300

    comment of excercise and fix some problems

--- clase7/alumno.py
+++ clase7/alumno.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env/ python3
 #-*- coding: utf-8 -*-
 
+"""Ejercicio: Hacer un nuevo módulo, llamado alumno.py que tenga como atributos un
+número de legajo, tres notas parciales y una nota promedio. Hacer un método que permita
+calcular y mostrar el promedio del alumno según las tres calificaciones parciales. Realizar
+otro método que permita mostrar el estado del alumno según la calificación promedio
+(menor que 6 es alumno libre, entre 6 y 8 alumno regular y mayor a 8 alumno
+promocionado)."""
+
 class Alumno:
-    def __init__(self, nlegajo, *notas):
+    def __init__(self, nlegajo, n1, n2, n3):
         self.nlegajo = nlegajo
-        self.notas_parciales = *notas
-        self.nota_promedio = nota_promedio()
+        self.notas_parciales = (n1, n2, n3)
+        self.promedio = self.nota_promedio()
     def nota_promedio(self):
         total = 0
         for i in self.notas_parciales:
             total += i
         return total/len(self.notas_parciales)
     def estado_alumno(self):
-        if self.nota_promedio < 6:
-            print('Alumno libre')
-        elif self.nota_promedio <= 8:
-            print('Alumno regular')
+        if self.promedio < 6:
+            print(f'Alumno con legajo {self.nlegajo}: libre')
+        elif self.promedio <= 8:
+            print(f'Alumno con legajo {self.nlegajo}: regular')
         else:
-            print('Alumno promocionado')
+            print(f'Alumno con legajo {self.nlegajo}: promocionado')
