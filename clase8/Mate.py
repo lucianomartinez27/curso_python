5a6dab5f623af2fe169f2d8f0c5447519071b07a
Luciano Martinez <lucianomartinez@gmail.com>
2020-06-08 15:35:14 -0300

    ejercicio clase 8 - POO (clase Mate)

---
+++ clase8/Mate.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+
+class Mate:
+    """El objeto de tipo mate describe las características como también
+    el funcionamiento de la infusión rioplatense"""
+
+    def __init__(self, n):
+        self.cebadas_disponibles = n
+        self.lleno = False
+    
+    def cebar(self):
+        if self.lleno:
+            print('¡Cuidado! ¡Te quemaste!')
+        else:
+            self.lleno = True
+            print('Cebando')
+    
+    def beber(self):
+        if self.lleno:
+            if self.cebadas_disponibles == 0:
+                print('Advertencua: El mate está lavado')
+            else:
+                self.lleno = False
+                self.cebadas_disponibles -= 1
+                print('Qué rico mate')
+        else:
+            print('El mate está vacío!')
+
+    def arreglar(self, cebadas_adicionales):
+        """ Agrega a las cebadas disponibles la cantidad que se pase como parámetro"""
+        self.cebadas_disponibles = cebadas_adicionales
+
+    def __str__(self):
+        if self.cebadas_disponibles:
+            return 'El mate aún se puede usar'
+        return 'El mate ya no se puede usar'
+
+    def __eq__(self, otro_mate):
+        return self.cebadas_disponibles == otro_mate.cebadas_disponibles \
+               and self.lleno == otro_mate.lleno
+
+    def __ne__(self, otro_mate):
+        return self.cebadas_disponibles != otro_mate.cebadas_disponibles \
+               or self.lleno != otro_mate.lleno
+    
+    def __lt__(self, otro_mate):
+        return self.cebadas_disponibles > otro_mate.cebadas_disponibles \
+                or self.lleno > otro_mate.lleno
+
+
---
+++ clase8/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+from Mate import Mate
+
+mi_mate = Mate(5)
+otro_mate = Mate(6)
+
+mi_mate == otro_mate
+mi_mate != otro_mate
+mi_mate > otro_mate
+mi_mate < otro_mate
+
+mi_mate.beber()
+mi_mate.cebar()
+mi_mate.cebar()
+mi_mate.beber()
+
+mi_mate.arreglar(5)
+
+
+print(mi_mate)
+print(otro_mate)
